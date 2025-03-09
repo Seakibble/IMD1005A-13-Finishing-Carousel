49bfef137c8c13ca423d410ccd7577f536b1cc23
Seakibble <max.leeming@gmail.com>
2025-03-09 16:52:12 -0400

    Comments added

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,11 +1,17 @@
+// Dark mode is very simple, script-wise. The premise is that when we click the button, we apply a class to body, and that in turn styles the page differently. So the clever stuff is in our stylesheet. 
 function ToggleDarkMode() {
+    // First we toggle a class on the body. We can access it with document.body
+    // .toggle() will apply a class if is missing, or remove it is present. 
     document.body.classList.toggle('darkMode')
 
+    // Here we're just grabbing a quick reference to the button.
     let $btn = document.getElementById('btn_darkmode')
-
+    // Then we check to see if we're currently in dark mode after the button is pressed.
     if (document.body.classList.contains('darkMode')) {
+        // If we are, then we need to alter the button to show the light mode icon (since the button now changes to light mode).
         $btn.innerText = 'light_mode'
     } else {
+        // Otherwise, we'll go back to darkmode.
         $btn.innerText = 'dark_mode'
     }
 }
